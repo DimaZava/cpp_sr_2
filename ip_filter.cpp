1f90af4e5615c836608c21a10d1a8451f952086c
dimazava <dimazava@hotmail.com>
2023-03-22 22:21:59 +0300

    Replaced map with vector implementation
    Improved throw feedback mechanism

--- ip_filter.cpp
+++ ip_filter.cpp
@@ -11,17 +11,18 @@
 #include <limits>
 #include <cmath>
 #include <string>
+#include <sstream>
 
 #define UNUSED(variable) (void)variable
 
 class ip_address;
 
 int perform_read_and_sort(const char **argv);
-int prepare_data(const char **argv, std::multimap<uint, ip_address, std::greater<uint> > &general_sorted_ips);
-void perform_task_one(std::multimap<uint, ip_address, std::greater<uint> > &general_sorted_ips);
-void perform_task_two(std::multimap<uint, ip_address, std::greater<uint> > &general_sorted_ips);
-void perform_task_three(std::multimap<uint, ip_address, std::greater<uint> > &general_sorted_ips);
-void perform_task_four(std::multimap<uint, ip_address, std::greater<uint> > &general_sorted_ips);
+void prepare_data_vector(const char **argv, std::vector<ip_address> &general_sorted_ips_vector);
+void perform_task_one_vector(const std::vector<ip_address> &general_sorted_ips_vector);
+void perform_task_two_vector(const std::vector<ip_address> &general_sorted_ips_vector);
+void perform_task_three_vector(const std::vector<ip_address> &general_sorted_ips_vector);
+void perform_task_four_vector(const std::vector<ip_address> &general_sorted_ips_vector);
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -48,23 +49,47 @@ std::vector<std::string> split(const std::string &str, char d)
     return r;
 }
 
-class ip_address {
+class ip_address
+{
 public:
+    uint normalized_ip;
     std::string string_representation;
     std::vector<uint> octets;
     
-    ip_address(std::string string_representation) {
-        this->string_representation = string_representation;
+    ip_address(std::vector<std::vector<std::string> >::iterator iterator) {
+        // create normalized ip key
+        uint index = 4; // starting index = power from 4 for algorithm convenience down to 0
+        auto ip_normalizer = [&index](uint partial_result, std::string item) {
+            uint ip_part = uint(std::stoul(item));
+            index--;
+            return partial_result + int(pow(256.0, index)) * ip_part;
+        };
+        this->normalized_ip = std::reduce(iterator->begin(), iterator->end(), 0, ip_normalizer);
+        
+        // configure readable ip address
+        auto ip_formatter = [](std::string partial_result, std::string item) {
+            return partial_result + (!partial_result.empty() ? "." : "") + item;
+        };
+        this->string_representation = std::reduce(iterator->begin(), iterator->end(), std::string(), ip_formatter);
         
         std::vector<std::string> string_vector = split(string_representation, '.');
         auto octets_formatter = [](std::string item) {
             return uint(std::stoul(item));
         };
         std::transform(string_vector.begin(), string_vector.end(), std::back_inserter(octets), octets_formatter);
-        // std::vector<uint> test = std::vector<uint>(string_vector.begin(), string_vector.end(), [](std::string item) {
-        //     return uint(std::stoul(item));
-        // });
-        // this->octets
+    }
+    
+    friend std::ostream &operator<<(std::ostream &os, const ip_address &ip_address)
+    {
+        auto octets_formatter = [](std::string partial_result, uint item)
+        {
+            return partial_result + std::to_string(item);
+        };
+        
+        os << "normalized_ip: " << ip_address.normalized_ip << std::endl
+        << "string_representation: " << ip_address.string_representation << std::endl
+        << "octets: " << std::reduce(ip_address.octets.begin(), ip_address.octets.end(), std::string(), octets_formatter) << std::endl;
+        return os;
     }
 };
 
@@ -87,22 +112,20 @@ int main(int argc, char const *argv[])
 
 int perform_read_and_sort(const char **argv) {
     // read data
-    std::multimap<uint, ip_address, std::greater<uint> > general_sorted_ips;
-    if (prepare_data(argv, general_sorted_ips) == -1) {
-        return  -1;
-    }
-    perform_task_one(general_sorted_ips);
-    perform_task_two(general_sorted_ips);
-    perform_task_three(general_sorted_ips);
-    perform_task_four(general_sorted_ips);
-    // std::cout << "Voila, finish!" << std::endl;
+    std::vector<ip_address> general_sorted_ips_vector;
+    
+    prepare_data_vector(argv, general_sorted_ips_vector);
+    perform_task_one_vector(general_sorted_ips_vector);
+    perform_task_two_vector(general_sorted_ips_vector);
+    perform_task_three_vector(general_sorted_ips_vector);
+    perform_task_four_vector(general_sorted_ips_vector);
     return 0;
 }
 
-int prepare_data(const char **argv, std::multimap<uint, ip_address, std::greater<uint> > &general_sorted_ips) {
+void prepare_data_vector(const char **argv, std::vector<ip_address> &general_sorted_ips_vector) {
     std::string ip_file_path = (std::string)argv[1];
     if (ip_file_path.find("ip_filter.tsv") == std::string::npos) {
-        return -1;
+        throw std::runtime_error("Error: Please, pass ip_filter.tsv file as parameter");
     }
     
     // parse
@@ -110,131 +133,92 @@ int prepare_data(const char **argv, std::multimap<uint, ip_address, std::greater
     std::vector<std::vector<std::string> > ip_container;
     if (file_stream.is_open()) {
         std::string ip;
-        // int extra1, extra2;
-        // if (addressesFile.ignore(numeric_limits<streamsize>::max(), addressesFile.widen('\n'))){
         while (file_stream >> ip) {
             file_stream.ignore(std::numeric_limits<std::streamsize>::max(), file_stream.widen('\n'));
             ip_container.push_back(split(ip, '.'));
         }
     }
     
-    // print parsed data
-    // std::cout << "--------------------" << std::endl;
-    // std::cout << "Input Data" << std::endl << "Total Count: " << ip_container.size() << std::endl;
-    // for (auto iterator = ip_container.begin(); iterator != ip_container.end(); ++iterator) {
-    //     for (auto ip_iterator = iterator->begin(); ip_iterator != iterator->end(); ++ip_iterator) {
-    //         std::cout << *ip_iterator;
-    //         if (ip_iterator != --iterator->end()) {
-    //             std::cout << ".";
-    //         }
-    //     }
-    //     std::cout << std::endl;
-    // }
-    
-    // perform general sort in the way of fullfill the 'ordered' map
-    
+    // perform general sort in the way of fullfill the vector
     for (auto it = ip_container.begin(); it != ip_container.end(); ++it) {
-        // configure readable ip address
-        auto ip_formatter = [](std::string partial_result, std::string item) {
-            return partial_result + (!partial_result.empty() ? "." : "") + item;
-        };
-        std::string ip_string = std::reduce(it->begin(), it->end(), std::string(), ip_formatter);
-        
-        // create normalized ip key
-        uint index = 4; // starting index = power from 4 for algorithm convenience down to 0
-        auto ip_normalizer = [&index](uint partial_result, std::string item) {
-            uint ip_part = uint(std::stoul(item));
-            index--;
-            return partial_result + int(pow(256.0, index)) * ip_part;
-        };
-        uint normalized_ip = std::reduce(it->begin(), it->end(), 0, ip_normalizer);
-        
-        general_sorted_ips.insert({normalized_ip, ip_address(ip_string)});
-        // auto result = general_sorted_ips.insert({normalized_ip, ip_string});
-        // if (!result.second) {
-        //     std::cout << "wow wow" << ip_string << ":" << normalized_ip << std::endl;
-        // }
-        // std::cout << "wow wow" << ip_string << ":" << normalized_ip << std::endl;
+        try
+        {
+            general_sorted_ips_vector.push_back(ip_address(it));
+        }
+        catch(const std::exception &e)
+        {
+            std::ostringstream errorStream;
+            errorStream << "Error: " << std::string(e.what()) << std::endl <<
+            "Errneous item at: " << std::to_string(it - ip_container.begin()) << std::endl;
+            throw std::runtime_error(errorStream.str());
+        }
     }
-    return 0;
-}
-
-void perform_task_one(std::multimap<uint, ip_address, std::greater<uint> > &general_sorted_ips) {
-    // std::cout << "--------------------" << std::endl;
-    // std::cout << "Task #1 General Sorted Data:" << std::endl << "Total Count: " << general_sorted_ips.size() << std::endl;
-    for (auto it = general_sorted_ips.begin(); it != general_sorted_ips.end(); ++ it) {
-        // std::cout << it->first << "\t" << it->second.string_representation << std::endl;
-        std::cout << it->second.string_representation << std::endl;
-        logs.append(it->second.string_representation + '\n');
-    }
-}
-
-void perform_task_two(std::multimap<uint, ip_address, std::greater<uint> > &general_sorted_ips) {
-    // std::cout << "--------------------" << std::endl;
-    // std::cout << "Task #2 First Octet == 1:" << std::endl;
     
-    std::multimap<uint, ip_address, std::greater<uint> > task_two_ips;
-    auto octet_one_filter = [](std::pair<uint, ip_address> ip_address_pair) {
-        return ip_address_pair.second.octets.at(0) == 1;
+    auto comparator = [](const ip_address &lhs, const ip_address &rhs) -> bool {
+        return lhs.normalized_ip > rhs.normalized_ip;
+    };
+    
+    std::sort(general_sorted_ips_vector.begin(), general_sorted_ips_vector.end(), comparator);
+}
+
+void perform_task_one_vector(const std::vector<ip_address> &general_sorted_ips_vector) {
+    // print parsed data
+    for (auto element : general_sorted_ips_vector) {
+        std::cout << element.string_representation << std::endl;
+        logs.append(element.string_representation + '\n');
+    }
+}
+
+void perform_task_two_vector(const std::vector<ip_address> &general_sorted_ips_vector) {
+    std::vector<ip_address> task_two_ips_vector;
+    auto octet_one_filter = [](ip_address ip_address) {
+        return ip_address.octets.at(0) == 1;
     };
     std::copy_if(
-                 general_sorted_ips.begin(),
-                 general_sorted_ips.end(),
-                 std::inserter(task_two_ips, task_two_ips.end()),
+                 general_sorted_ips_vector.begin(),
+                 general_sorted_ips_vector.end(),
+                 std::back_inserter(task_two_ips_vector),
                  octet_one_filter
                  );
-    for (auto it = task_two_ips.begin(); it != task_two_ips.end(); ++ it) {
-        // std::cout << it->first << "\t" << it->second.string_representation << std::endl;
-        std::cout << it->second.string_representation << std::endl;
-        logs.append(it->second.string_representation + '\n');
+    for (auto element : task_two_ips_vector) {
+        std::cout << element.string_representation << std::endl;
+        logs.append(element.string_representation + '\n');
     }
-    // std::cout << "Total Count: " << task_two_ips.size() << std::endl;
 }
 
-void perform_task_three(std::multimap<uint, ip_address, std::greater<uint> > &general_sorted_ips) {
-    // std::cout << "--------------------" << std::endl;
-    // std::cout << "Task #3 First Octet == 46, Second Octet == 70:" << std::endl;
-    
-    std::multimap<uint, ip_address, std::greater<uint> > task_three_ips;
-    auto octet_46_70_filter = [](std::pair<uint, ip_address> ip_address_pair) {
-        return ip_address_pair.second.octets.at(0) == 46 &&
-        ip_address_pair.second.octets.at(1) == 70;
+void perform_task_three_vector(const std::vector<ip_address> &general_sorted_ips_vector) {
+    std::vector<ip_address> task_three_ips_vector;
+    auto octet_46_70_filter = [](ip_address ip_address) {
+        return ip_address.octets.at(0) == 46 && ip_address.octets.at(1) == 70;
     };
     std::copy_if(
-                 general_sorted_ips.begin(),
-                 general_sorted_ips.end(),
-                 std::inserter(task_three_ips, task_three_ips.end()),
+                 general_sorted_ips_vector.begin(),
+                 general_sorted_ips_vector.end(),
+                 std::back_inserter(task_three_ips_vector),
                  octet_46_70_filter
                  );
-    for (auto it = task_three_ips.begin(); it != task_three_ips.end(); ++ it) {
-        // std::cout << it->first << "\t" << it->second.string_representation << std::endl;
-        std::cout << it->second.string_representation << std::endl;
-        logs.append(it->second.string_representation + '\n');
+    for (auto element : task_three_ips_vector) {
+        std::cout << element.string_representation << std::endl;
+        logs.append(element.string_representation + '\n');
     }
-    // std::cout << "Total Count: " << task_three_ips.size() << std::endl;
 }
 
-void perform_task_four(std::multimap<uint, ip_address, std::greater<uint> > &general_sorted_ips) {
-    // std::cout << "--------------------" << std::endl;
-    // std::cout << "Task #4 Any Octet == 46:" << std::endl;
-    
-    std::multimap<uint, ip_address, std::greater<uint> > task_four_ips;
-    auto octet_46_70_filter = [](std::pair<uint, ip_address> ip_address_pair) {
-        return ip_address_pair.second.octets.at(0) == 46 ||
-        ip_address_pair.second.octets.at(1) == 46 ||
-        ip_address_pair.second.octets.at(2) == 46 ||
-        ip_address_pair.second.octets.at(3) == 46;
+void perform_task_four_vector(const std::vector<ip_address> &general_sorted_ips_vector) {
+    std::vector<ip_address> task_four_ips_vector;
+    auto octet_46_70_filter = [](ip_address ip_address) {
+        return ip_address.octets.at(0) == 46 ||
+        ip_address.octets.at(1) == 46 ||
+        ip_address.octets.at(2) == 46 ||
+        ip_address.octets.at(3) == 46;
     };
     std::copy_if(
-                 general_sorted_ips.begin(),
-                 general_sorted_ips.end(),
-                 std::inserter(task_four_ips, task_four_ips.end()),
+                 general_sorted_ips_vector.begin(),
+                 general_sorted_ips_vector.end(),
+                 std::back_inserter(task_four_ips_vector),
                  octet_46_70_filter
                  );
-    for (auto it = task_four_ips.begin(); it != task_four_ips.end(); ++ it) {
-        // std::cout << it->first << "\t" << it->second.string_representation << std::endl;
-        std::cout << it->second.string_representation << std::endl;
-        logs.append(it->second.string_representation + '\n');
+    for (auto element : task_four_ips_vector) {
+        std::cout << element.string_representation << std::endl;
+        logs.append(element.string_representation + '\n');
     }
-    // std::cout << "Total Count: " << task_four_ips.size() << std::endl;
 }
